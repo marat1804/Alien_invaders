e7e77981f303882510a1124b4fed3abfb352229b
Marat <maretcoop11@gmail.com>
2020-05-29 20:43:52 +0300

    Added aliens

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     ship = Ship(screen, ai_settings)
     bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     pygame.display.set_caption('Alien Invasion')
     while True:
         gf.check_events(ship, ai_settings, screen, bullets)
         ship.updete()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
         pygame.display.flip()
 
 
